7be78c25cfc66d1d9e1a9f2c5c44263fdbb3d4f6
David J. Hammaker <davidjosh93@gmail.com>
2019-02-02 01:59:24 -0500

    Add physical qualities to 'base.html'
    Uses JavaScript to generate random qualities, then presents the 
    qualities in a stylized list.

--- static/main.js
+++ static/main.js
@@ -1,30 +1,33 @@
-genders = ['male',
-           'female'];
+genders = ['Male', 'Female'];
 
-heights = ['very short',
-           'short',
-           'average',
-           'tall',
-           'very tall'];
+heights = ['Very short', 'Short', 'Average', 'Tall', 'Very tall'];
 
-weights = ['underweight',
-           'normal',
-           'overweight'];
+weights = ['Underweight', 'Average', 'Overweight'];
 
-hair_colors = ['black',
-               'dark brown',
-               'brown',
-               'light brown',
-               'dirty blond',
-               'blond',
-               'strawberry blond',
-               'red',
-               'auburn',
-               'white',
-               'silver'];
+hairColors = ['Black', 'Dark brown', 'Brown', 'Light brown', 'Dirty blond', 'Blond', 'Strawberry blond', 'Red', 'Auburn', 'White', 'Silver'];
 
-eye_colors = ['brown',
-              'blue',
-              'green',
-              'gray',
-              'hazel'];
+eyeColors = ['Brown', 'Blue', 'Green', 'Gray', 'Hazel'];
+
+function assignAttribute(attributeList, attributeElement) {
+    let attribute = attributeList[Number.parseInt((Math.random() * attributeList.length))];
+    attributeElement.textContent = attribute;
+};
+
+function select() {
+    let genderElement = document.getElementById('gender');
+    assignAttribute(genders, genderElement);
+
+    let heightElement = document.getElementById('height');
+    assignAttribute(heights, heightElement);
+
+    let weightElement = document.getElementById('weight');
+    assignAttribute(weights, weightElement);
+
+    let hairColorElement = document.getElementById('hair_color');
+    assignAttribute(hairColors, hairColorElement);
+
+    let eyeColorElement = document.getElementById('eye_color');
+    assignAttribute(eyeColors, eyeColorElement);
+};
+
+select();
