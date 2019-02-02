c32945ff4b694cfb212919e464ab7e1be44e531d
David J. Hammaker <davidjosh93@gmail.com>
2019-02-02 00:48:12 -0500

    Temporarily use dummy name instead of API name
    Behind The Name permits a limited number of requests. While this number 
    is probably too large to matter, I'd rather not take chances. A dummy 
    name will be used for now.

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    api_key = os.environ.get('BTN_KEY')
-    gender = 'f'
-    random_name_url = f'https://www.behindthename.com/api/random.json' \
-        f'?usage=eng&number=1&randomsurname=yes&gender={gender}' \
-        f'&key={api_key}'
-    names_request = requests.get(random_name_url)
-    names_list = names_request.json()['names']
-    name = f'Name: {names_list[0]} {names_list[1]}'
+    # api_key = os.environ.get('BTN_KEY')
+    # gender = 'f'
+    # random_name_url = f'https://www.behindthename.com/api/random.json' \
+    #     f'?usage=eng&number=1&randomsurname=yes&gender={gender}' \
+    #     f'&key={api_key}'
+    # names_request = requests.get(random_name_url)
+    # names_list = names_request.json()['names']
+    # name = f'Name: {names_list[0]} {names_list[1]}'
+    name = 'John Doe'
     return render_template('base.html', name=name)
 
 
