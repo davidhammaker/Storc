0ce10892772fd82ebe661a574310e3db3accb103
David J. Hammaker <davidjosh93@gmail.com>
2019-02-02 11:32:46 -0500

    Modify comments in 'app.py'
    Only comment out what we have to, and provide comments to clarify what's 
    happening.

--- app.py
+++ app.py
@@ -7,14 +7,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # api_key = os.environ.get('BTN_KEY')
-    # gender = 'f'
-    # random_name_url = f'https://www.behindthename.com/api/random.json' \
-    #     f'?usage=eng&number=1&randomsurname=yes&gender={gender}' \
-    #     f'&key={api_key}'
+    api_key = os.environ.get('BTN_KEY')
+    gender = 'f'
+    random_name_url = f'https://www.behindthename.com/api/random.json' \
+        f'?usage=eng&number=1&randomsurname=yes&gender={gender}' \
+        f'&key={api_key}'
+
+    # In the future, the next 3 lines will be un-commented:
     # names_request = requests.get(random_name_url)
     # names_list = names_request.json()['names']
     # name = f'Name: {names_list[0]} {names_list[1]}'
+
+    # Temporary name:
     name = 'John Doe'
     return render_template('base.html', name=name)
 
