53ea394826ca46bc84ef6359c841b12c2098cd68
David J. Hammaker <davidjosh93@gmail.com>
2019-02-02 01:03:45 -0500

    Create 'main.js' with common physical attributes
    The script is included in 'base.html'.

---
+++ static/main.js
@@ -0,0 +1,30 @@
+genders = ['male',
+           'female'];
+
+heights = ['very short',
+           'short',
+           'average',
+           'tall',
+           'very tall'];
+
+weights = ['underweight',
+           'normal',
+           'overweight'];
+
+hair_colors = ['black',
+               'dark brown',
+               'brown',
+               'light brown',
+               'dirty blond',
+               'blond',
+               'strawberry blond',
+               'red',
+               'auburn',
+               'white',
+               'silver'];
+
+eye_colors = ['brown',
+              'blue',
+              'green',
+              'gray',
+              'hazel'];
