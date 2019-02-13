6862512d6656c2f3ae6a13214bfb7e82d3730947
David J. Hammaker <davidjosh93@gmail.com>
2019-02-13 02:36:43 -0500

    Modify EmailRegistrationForm validation
    Require passwords to be at least 6 characters long, and ensure that 
    username and email uniqueness is validated.

--- storc/forms.py
+++ storc/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import (
     StringField, PasswordField, BooleanField, SubmitField)
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import (
+    DataRequired, Length, Email, EqualTo, ValidationError)
+from storc.models import User
 
 
 class EmailRegistrationForm(FlaskForm):
@@ -15,11 +17,22 @@ class EmailRegistrationForm(FlaskForm):
         Email()])
     password = PasswordField('Password', validators=[
         DataRequired(),
-        Length(max=72)])
+        Length(min=6, max=72)])
     confirm_password = PasswordField('Confirm Password', validators=[
         DataRequired(),
         EqualTo('password')])
     submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already in use.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                'That email address is already in use.')
 
 
 class EmailLoginForm(FlaskForm):
