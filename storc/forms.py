a24a01f8ddfb3885608cb9df5c82727ad69d8043
David J. Hammaker <davidjosh93@gmail.com>
2019-02-13 00:11:02 -0500

    Create registration and login forms for email
    Create FlaskForm classes for registering and logging in users with email 
    (rather than a 3rd party OAuth2 provider).

---
+++ storc/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField, PasswordField, BooleanField, SubmitField)
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class EmailRegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[
+        Length(max=32)])
+    username = StringField('Username', validators=[
+        DataRequired(),
+        Length(min=2, max=32)])
+    email = StringField('Email Address', validators=[
+        DataRequired(),
+        Email()])
+    password = PasswordField('Password', validators=[
+        DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(),
+        EqualTo('password')])
+    submit = SubmitField('Submit')
+
+
+class EmailLoginForm(FlaskForm):
+    email = StringField('Email Address', validators=[
+        DataRequired(),
+        Email()])
+    password = PasswordField('Password', validators=[
+        DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
