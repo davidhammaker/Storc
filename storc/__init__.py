ff95bceab5eff29822257e672db32186d620271f
David J. Hammaker <davidjosh93@gmail.com>
2019-02-13 13:10:05 -0500

    Configure application email settings

--- storc/__init__.py
+++ storc/__init__.py
@@ -2,12 +2,19 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('STORC_DB')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('STORC_SECRET')
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('STORC_EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('STORC_EMAIL_PASS')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+mail = Mail(app)
 
 from storc import views
