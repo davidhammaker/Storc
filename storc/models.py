299e0669d644ce7271786de0502ab4ac8f654a9d
David J. Hammaker <davidjosh93@gmail.com>
2019-02-13 13:11:55 -0500

    Create 'get_token' and 'validate_token' methods
    Methods apply to User model, and will be used for verifying email 
    addresses and resetting passwords.

--- storc/models.py
+++ storc/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from storc import db
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from storc import app, db
 
 
 class Character(db.Model):
@@ -20,4 +21,18 @@ class User(db.Model):
     about_me = db.Column(db.Text)
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
+    validated = db.Column(db.Boolean)
     characters = db.relationship('Character', backref='user', lazy=True)
+
+    def get_token(self, expire=1800):
+        s = Serializer(app.config['SECRET_KEY'], expire)
+        return s.dumps({'user_id': self.id})
+
+    @staticmethod
+    def validate_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
