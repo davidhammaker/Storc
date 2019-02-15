3fc3d6608826a5282ec44209ad8ef487b7235bdb
David J. Hammaker <davidjosh93@gmail.com>
2019-02-15 14:33:54 -0500

    Remove development naming and use Behind the Name
    The application has reached a point in development where random names 
    are more useful than 'John Doe'.

--- storc/views.py
+++ storc/views.py
@@ -31,18 +31,13 @@ def home():
 @app.route('/new_character')
 def new_character():
     gender = choice(['male', 'female'])
-
-    # The following lines have been commented out for development:
-    # api_key = os.environ.get('BTN_KEY')
-    # random_name_url = f'https://www.behindthename.com/api/random.json' \
-    #     f'?usage=eng&number=1&randomsurname=yes&gender={gender[0]}' \
-    #     f'&key={api_key}'
-    # names_request = requests.get(random_name_url)
-    # names_list = names_request.json()['names']
-    # name = f'{names_list[0]} {names_list[1]}'
-
-    # Temporary name for development:
-    name = 'John Doe'
+    api_key = os.environ.get('BTN_KEY')
+    random_name_url = f'https://www.behindthename.com/api/random.json' \
+        f'?usage=eng&number=1&randomsurname=yes&gender={gender[0]}' \
+        f'&key={api_key}'
+    names_request = requests.get(random_name_url)
+    names_list = names_request.json()['names']
+    name = f'{names_list[0]} {names_list[1]}'
     return render_template('new_character.html', name=name,
                            gender=gender.title())
 
