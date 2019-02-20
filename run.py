31c047f34d38cb672ef7cdd5ad4de1b6c3c88e4a
David J. Hammaker <davidjosh93@gmail.com>
2019-02-20 17:06:54 -0500

    Enable database setup
    When a developer includes the '--setup' flag while running the 
    application, the application first creates the database.

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
-from storc import create_app
+import sys
+from storc import create_app, db
 
 
 app = create_app()
 
 
 if __name__ == '__main__':
+    if '--setup' in sys.argv:
+        app.app_context().push()
+        db.create_all()
     app.run(debug=True)
