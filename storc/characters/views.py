eb3703131ba8306ae6187d4f4c4de84623ecca4b
David J. Hammaker <davidjosh93@gmail.com>
2019-02-20 16:49:18 -0500

    Improve appearance of character saving
    Formerly, after saving a character, the user would be redirected to the 
    home page, and the new character (and flash message about the new 
    character) would not show up until the next refresh or other navigation. 
    Now, a JavaScript timeout delays the redirect for 200 milliseconds 
    before sending the user to 'save_character', which then redirects the 
    user to the appropriate character page. At the character page, the flash 
    message shows up as expected.

--- storc/characters/views.py
+++ storc/characters/views.py
@@ -1,7 +1,8 @@
 import os
 from random import choice
 import requests
-from flask import render_template, request, flash, Blueprint
+from flask import (
+    render_template, request, flash, Blueprint, redirect, url_for)
 from flask_login import current_user
 from storc import db
 from storc.models import Character
@@ -20,38 +21,51 @@ def new_character():
     names_request = requests.get(random_name_url)
     names_list = names_request.json()['names']
     name = f'{names_list[0]} {names_list[1]}'
-    return render_template('new_character.html', name=name,
-                           gender=gender.title())
+    character_last = \
+        Character.query.order_by(Character.date.desc()).first()
+    return render_template(
+        'new_character.html',
+        name=name,
+        gender=gender.title(),
+        id=(character_last.id + 1))
 
 
-@characters.route('/save_character', methods=['POST'])
+@characters.route('/save_character', methods=['GET', 'POST'])
 def save_character():
-    new_character = request.form
-    character = Character(
-        name=new_character['name'],
-        gender=new_character['gender'],
-        height=new_character['height'],
-        weight=new_character['weight'],
-        hair_color=new_character['hair_color'],
-        eye_color=new_character['eye_color'],
-        unique_attribute=new_character['unique_attribute'],
-        favorite_clothes=new_character['favorite_clothes'],
-        hair=new_character['hair'],
-        mannerism_one=new_character['mannerism_one'],
-        mannerism_two=new_character['mannerism_two'],
-        speaking_style=new_character['speaking_style'],
-        skill=new_character['skill'],
-        flaw=new_character['flaw'],
-        fear=new_character['fear'],
-        favorite=new_character['favorite'],
-        family=new_character['family'],
-        friends=new_character['friends'],
-        significant_other=new_character['significant_other'],
-        user=current_user)
-    db.session.add(character)
-    db.session.commit()
-    flash('Your character has been saved!', 'good')
-    return 'Character saved successfully.'
+    if request.method == 'POST':
+        new_character = request.form
+        character = Character(
+            name=new_character['name'],
+            gender=new_character['gender'],
+            height=new_character['height'],
+            weight=new_character['weight'],
+            hair_color=new_character['hair_color'],
+            eye_color=new_character['eye_color'],
+            unique_attribute=new_character['unique_attribute'],
+            favorite_clothes=new_character['favorite_clothes'],
+            hair=new_character['hair'],
+            mannerism_one=new_character['mannerism_one'],
+            mannerism_two=new_character['mannerism_two'],
+            speaking_style=new_character['speaking_style'],
+            skill=new_character['skill'],
+            flaw=new_character['flaw'],
+            fear=new_character['fear'],
+            favorite=new_character['favorite'],
+            family=new_character['family'],
+            friends=new_character['friends'],
+            significant_other=new_character['significant_other'],
+            user=current_user)
+        db.session.add(character)
+        db.session.commit()
+        flash('Your character has been saved!', 'good')
+        return 'Character saved successfully.'
+    else:
+        character_last = Character.query\
+            .order_by(Character.date.desc())\
+            .filter_by(user_id=current_user.id)\
+            .first()
+        return redirect(
+            url_for('characters.character', id=character_last.id))
 
 
 @characters.route('/character/<int:id>')
