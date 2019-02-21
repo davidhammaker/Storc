c4bf8ebf4bdc7cba866bf6c1005e3097e231b2f3
David J. Hammaker <davidjosh93@gmail.com>
2019-02-21 11:23:36 -0500

    Modify use of 'app_context' per documentation

--- run.py
+++ run.py
@@ -7,6 +7,6 @@ app = create_app()
 
 if __name__ == '__main__':
     if '--setup' in sys.argv:
-        app.app_context().push()
-        db.create_all()
+        with app.app_context():
+            db.create_all()
     app.run(debug=True)
